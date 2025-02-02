abf3e9c4d7e4199af915793923e8ca1067161add
Aniruddha Atale <aniruddha.atale@klaviyo.com>
2025-02-01 21:01:13 -0500

    async migration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-#![allow(unused)]
-use codecrafters_dns_server::{server::DNSServer};
+use codecrafters_dns_server::server::DNSServer;
+use std::process;
 
-
-fn main() {
-    let server = DNSServer::new("127.0.0.1:2053");
-    match server.run() {
+#[tokio::main]
+async fn main() {
+    let mut server = DNSServer::new("127.0.0.1:2053");
+    match server.run().await {
         Ok(()) => {println!("Should not reach here");}
-        Err(e) => {println!("Failed to start Server");}
+        Err(e) => {
+            eprintln!("Problem parsing arguments: {e}");
+            process::exit(1);
+        }
     }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use std::net::UdpSocket;
 use std::net::Ipv4Addr;
+use core::net::SocketAddr;
+use tokio::net::UdpSocket;
 use anyhow::Error;
 use anyhow::Result;
 
@@ -9,61 +10,73 @@ const DEFAULT_TTL: u32 = 3600;
 
 pub struct DNSServer {
     pub listen_addr: String,
+    conn: Option<UdpSocket>
 }
 
 impl DNSServer {
-
     pub fn new(addr: &str) -> Self {
         DNSServer {
             listen_addr: String::from(addr),
+            conn: None
         }
     }
 
-    pub fn run(&self) -> Result<()> {
-        let udp_socket = UdpSocket::bind(&self.listen_addr).expect("Failed to bind to address");
-        let mut buffer = [0; 512];
+    pub async fn run(&mut self) -> Result<()> {
+
+        match &self.conn {
+            Some(_conn) => return Err(Error::msg("Server already running")),
+            None => {
+                self.conn = Some(UdpSocket::bind(&self.listen_addr).await?);
+            }
+        }
+
+        let conn = self.conn.as_ref().expect("Socket not available");
+
         loop {
-            match udp_socket.recv_from(&mut buffer) {
-                Ok((_, source)) => {
-                    let buf = Vec::from(buffer);
-                    //print_hex(&buf);
-                    let request = DNSRequest::from_bytes(&buf).unwrap();
-                    //println!("Received {}", request);
-                    let mut answers = Vec::<RRecord>::new();
-                    let mut questions= Vec::<Question>::new();
-                    for q in request.questions {
-                        if q.qtype() == Type::A && q.qclass() == Class::IN {
-                            questions.push(Question::new(&q.qname(), q.qtype(), q.qclass()));
-                            let ip = Ipv4Addr::new(127, 0, 0, 1);
-                            let rdata = Vec::<u8>::from_iter(ip.octets());
-                            let answer = RRecord::new(q.qname(), Type::A, Class::IN, DEFAULT_TTL, rdata);
-                            answers.push(answer);
-                        }
-                    }
-    
-                    let response_header =
-                        Header::new_response_from_request(request.header, false, answers.len() as u16, 0, 0);
-    
-                    let response = DNSResponse::new(
-                        response_header,
-                        questions,
-                        answers,
-                        Vec::<RRecord>::new(),
-                        Vec::<RRecord>::new(),
-                    );
-                    //println!("Sending {}", response);
-                    let response = response.to_bytes().unwrap();
-                    //print_hex(&response);
-    
-                    udp_socket
-                        .send_to(&response, source)
-                        .expect("Failed to send response");
-                }
-                Err(e) => {
-                    return Err(Error::new(e));
+            let mut buf = [0u8; 512];
+            match conn.recv_from(&mut buf).await {
+                Ok((size, remote)) => {
+                    let msg_buf = Vec::from(&buf[..size]);
+                    self.process_request(msg_buf, &remote).await?;
+                },
+                Err(_e) => {
+                    return Err(Error::msg("Error receiving data from socket"));
                 }
             }
         }    
+    }
+
+    pub async fn process_request(&self, buf: Vec<u8>, remote_add: &SocketAddr) -> Result<()> {
+
+        let request = DNSRequest::from_bytes(&buf).unwrap();
+        let mut answers = Vec::<RRecord>::new();
+        let mut questions= Vec::<Question>::new();
+
+        for q in request.questions {
+            if q.qtype() == Type::A && q.qclass() == Class::IN {
+                questions.push(Question::new(&q.qname(), q.qtype(), q.qclass()));
+                let ip = Ipv4Addr::new(127, 0, 0, 1);
+                let rdata = Vec::<u8>::from_iter(ip.octets());
+                let answer = RRecord::new(q.qname(), Type::A, Class::IN, DEFAULT_TTL, rdata);
+                answers.push(answer);
+            }
+        }
+
+        let response_header =
+            Header::new_response_from_request(request.header, false, answers.len() as u16, 0, 0);
+
+        let response = DNSResponse::new(
+            response_header,
+            questions,
+            answers,
+            Vec::<RRecord>::new(),
+            Vec::<RRecord>::new(),
+        );
+
+        let response = response.to_bytes().unwrap();
+        let conn = self.conn.as_ref().expect("Socket not available");
+        conn.send_to(&response, remote_add).await?;
+        Ok(())
     }
 }
 
